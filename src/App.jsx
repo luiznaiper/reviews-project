a14f4f8b91a4118936edfd031fc71075a6f637c3
luiznaiper <luiz_naiper@hotmail.com>
2023-03-22 08:03:19 -0300

    :construction: Display card

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
 import { useState } from 'react'
 import people from './data'
-import { FaBeer } from 'react-icons/fa'
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa'
 function App() {
   const [index, setIndex] = useState(0)
   const { name, job, image, text } = people[index]
   console.log(name)
 
   return (
-    <h1>
-      Hola <FaBeer />
-    </h1>
+    <main>
+      <article className="review">
+        <div className="img-container">
+          <img src={image} alt={name} className="person-img" />
+          <span className="quote-icon">
+            <FaQuoteRight />
+          </span>
+        </div>
+        <h4 className="author">{name}</h4>
+        <p className="job">{job}</p>
+        <p className="info">{text}</p>
+      </article>
+    </main>
   )
 }
 
